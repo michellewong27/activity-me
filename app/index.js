9182009dd6a6ca3c7f2e0d79c0d242d2275b38e1
Michelle Wong <michellekwong2@gmail.com>
2020-10-22 13:30:36 -0400

    Implement descriptions in activities, added Nav Bar

--- app/index.js
+++ app/index.js
@@ -5,70 +5,73 @@
 
 // const e = require("express");
 
-function getActivities(){
+function getActivities() {
   fetch(`http://localhost:3000/activities`)
-  .then(function(body){return body.json()})
-  .then(function(activities){
-    activities.forEach(addActivity)
-  })
-};
+    .then(function (body) {
+      return body.json();
+    })
+    .then(function (activities) {
+      activities.forEach(addActivity);
+    });
+}
 getActivities();
 
-function persistScore(event){
+function persistScore(event) {
   fetch(`http://localhost:3000/activities/${event.target.dataset.id}`, {
-    method:"PATCH",
-    headers:{
+    method: "PATCH",
+    headers: {
       "content-type": "application/json",
-      accepts: "application/json"
+      accepts: "application/json",
     },
-    body: JSON.stringify({ score: event.target.dataset.score})
-  })
+    body: JSON.stringify({ score: event.target.dataset.score }),
+  });
   //for pessimistic rendering ->
   // .then(function(response){return response.json})
   // .then(function(data){changeScore(event)})
 }
 
-
-function addToActivities(activity){
+function addToActivities(activity) {
   fetch(`http://localhost:3000/activities`, {
-    method:"POST",
-    headers:{
-      "content-type":"application/json",
-      "accepts": "application/json"
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      accepts: "application/json",
     },
     body: JSON.stringify({
       id: activity.id,
       name: activity.name,
       img: activity.img,
-      score: activity.score
-    })
-  })
+      score: activity.score,
+      description: activity.description
+    }),
+  });
   addActivity(activity);
 }
 
 const parentUl = document.getElementsByClassName("activities")[0];
 const title = document.getElementsByClassName("title")[0];
-const addActivityBtn = document.createElement('button');
+const addActivityBtn = document.createElement("button");
 addActivityBtn.dataset.purpose = "form";
-addActivityBtn.innerText = "Add activity"
+addActivityBtn.innerText = "Add activity";
 title.appendChild(addActivityBtn);
 
-addActivityBtn.addEventListener("click", function(e){
+addActivityBtn.addEventListener("click", function (e) {
   addActivityBtn.remove();
   createForm(e);
-})
+});
 
-function createForm(e){
-  const form = document.createElement('form')
+function createForm(e) {
+  const form = document.createElement("form");
   form.dataset.purpose = "addActivityForm";
   form.innerHTML = `
   <input type="text" placeholder="Activity" name="name"/>
   <input type="text" placeholder="Image" name="img"/>
+  <input type="text" placeholder="Description" name="description" />
   <input type="submit" data-purpose="submitActivity"/>
   `;
   title.appendChild(form);
 
-  form.addEventListener("submit", function(e){
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
     let liCount = document.querySelectorAll("#activities li").length;
     let id = liCount + 1;
@@ -76,8 +79,9 @@ function createForm(e){
       id: id,
       name: e.target.name.value,
       img: e.target.img.value,
-      score: 0
-    }
+      score: 0,
+      description: e.target.description.value
+    };
     addToActivities(activity);
     form.remove();
     title.appendChild(addActivityBtn);
@@ -85,11 +89,12 @@ function createForm(e){
 }
 
 function addActivity(activity) {
-  console.log(activity)
+  console.log(activity);
   let newLi = document.createElement("li");
   newLi.className = "activity";
   newLi.innerHTML = `
   <h3>${activity.name}</h3>
+  <h4>${activity.description}</h4>
   <h4>Score: <span>${activity.score}</span> </h4>
   <img alt="" src=${activity.img} />
   <button class="upVote" data-purpose="increase" data-id=${activity.id} data-score=${activity.score}>Up Vote</button>
@@ -101,15 +106,20 @@ function addActivity(activity) {
 function changeScore(event) {
   let parentLi = event.target.parentNode;
   let span = parentLi.querySelector("span");
-  let score = event.target.dataset.purpose === "increase" ? parseInt(span.innerText) + 1 : parseInt(span.innerText) - 1 ;
+  let score =
+    event.target.dataset.purpose === "increase"
+      ? parseInt(span.innerText) + 1
+      : parseInt(span.innerText) - 1;
   event.target.dataset.score = score;
   span.innerText = score;
-};
+}
 
-parentUl.addEventListener("click", function(event){
-  if(event.target.dataset.purpose === "increase" || event.target.dataset.purpose === "decrease"){
+parentUl.addEventListener("click", function (event) {
+  if (
+    event.target.dataset.purpose === "increase" ||
+    event.target.dataset.purpose === "decrease"
+  ) {
     changeScore(event);
     persistScore(event);
   }
 });
-
